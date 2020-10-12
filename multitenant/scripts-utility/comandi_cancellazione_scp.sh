0b95658362021ec16200ac5160248ecaaff62ec6
Samuele Barzaghi <sbarzaghi@alteanet.it>
2020-10-12 21:51:17 +0200

    Rinominata mta id, app e saas application name

--- multitenant/scripts-utility/comandi_cancellazione_scp.sh
+++ multitenant/scripts-utility/comandi_cancellazione_scp.sh
@@ -7,4 +7,4 @@ cf dsk mtt-saas               mtt-portal-deployer-mtt-saas-credentials -f
 cf dsk mtt-xsuaa              mtt-portal-deployer-mtt-xsuaa-credentials -f
 cf dsk mtt-service-manager    sk-service-manager-service-instances -f
 
-cf undeploy mtt-mt --delete-services -f
+cf undeploy cloud-cold-chain-multitenant --delete-services -f
