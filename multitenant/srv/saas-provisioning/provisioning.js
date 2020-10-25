c3d75322fb9fe3687d685907950d9f41024a75c9
Samuele Barzaghi <sbarzaghi@alteanet.it>
2020-10-25 16:00:59 +0100

    Le dipendende vengono gestite dall'approuter Pulito il codice

--- multitenant/srv/saas-provisioning/provisioning.js
+++ multitenant/srv/saas-provisioning/provisioning.js
@@ -1,16 +1,4 @@
 module.exports = (service) => {
-	service.on("dependencies", async (req, next) => {
-		const res = await next();
-		const deps = [
-			{ xsappname: "6e011442-8e05-4078-a0d4-55e9dec06173!b13893|portal-cf-service!b1483" },
-		];
-		console.log(
-			"[INFO ][ON_GET_DEPENDENCIES] Dependent applications/services - : " +
-				JSON.stringify(deps),
-		);
-		return deps;
-	});
-
 	service.on("UPDATE", "tenant", async (req, next) => {
 		const res = await next(); // first call default implementation which is doing the HDI container creation
 		let url = `https://${req.data.subscribedSubdomain}-dev-cap-template-approuter.cfapps.us10.hana.ondemand.com`;
