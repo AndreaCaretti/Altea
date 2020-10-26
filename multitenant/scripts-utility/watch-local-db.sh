bdccb2d26c8e1cda41e117106fe8a2b78dbe0ce3
Samuele Barzaghi <sbarzaghi@alteanet.it>
2020-10-26 21:57:35 +0100

    Aggiunti test in locale con db in cloud

--- multitenant/scripts-utility/watch-local-db.sh
+++ multitenant/scripts-utility/watch-local-db.sh
@@ -6,6 +6,9 @@ set -o errexit
 
 echo -e "${GREEN}DB Locale${NC}"
 unset CDS_ENV
+# Scrive i log in console in modalità semplice esempio:
+# 2020-10-26T20:50:49.608Z - 200 - message
+export SIMPLE_LOG=true
 
 echo -e "${GREEN}Configurazione approuter per test in locale:${NC}"
 cp cloud-foundry/approuter/xs-app-local.json cloud-foundry/approuter/xs-app.json -v
