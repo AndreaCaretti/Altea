632498190c1ba94810c753bb2a9232d3940ca759
Samuele Barzaghi <sbarzaghi@alteanet.it>
2020-10-26 01:10:15 +0100

    Due watch, uno per db locale e uno per db remoto

--- multitenant/scripts-utility/watch-local-db.sh
+++ multitenant/scripts-utility/watch-local-db.sh
@@ -4,6 +4,9 @@ NC='\033[0m'
 
 set -o errexit
 
+echo -e "${GREEN}DB Locale${NC}"
+unset CDS_ENV
+
 echo -e "${GREEN}Configurazione approuter per test in locale:${NC}"
 cp cloud-foundry/approuter/xs-app-local.json cloud-foundry/approuter/xs-app.json -v
 cds watch & 
---
+++ multitenant/scripts-utility/watch-remote-db.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+set -o errexit
+
+echo -e "${GREEN}DB Remoto${NC}"
+export CDS_ENV=production
+
+echo -e "${GREEN}Configurazione approuter per test in locale:${NC}"
+cp cloud-foundry/approuter/xs-app-local.json cloud-foundry/approuter/xs-app.json -v
+
+cds watch & 
+
+cd cloud-foundry/approuter/
+npm start
+
+
+
+echo -e "${GREEN}Apri il browser all'indirizzo http://localhost:5000${NC}"
