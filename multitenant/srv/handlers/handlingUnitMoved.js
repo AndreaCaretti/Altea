7a3d2c386cd3c2f8256d20efeaa9e1f778127a18
Roberto De Salvo <rdesalvo@alteanet.it>
2020-10-27 21:38:43 +0100

    gestione handlingmovements da redis locale sul db hana

--- multitenant/srv/handlers/handlingUnitMoved.js
+++ multitenant/srv/handlers/handlingUnitMoved.js
@@ -3,41 +3,45 @@ const { promisify } = require("util");
 const redis = require("redis");
 
 class HandlingUnitMoved extends ZApplicationService {
-	async init() {
-		await super.init();
+  async init() {
+    await super.init();
 
-		console.log("Init HandlingUnitMoved.js");
+    console.log("Init HandlingUnitMoved.js");
 
-		const redisClient = redis.createClient();
+    const redisClient = redis.createClient();
 
-		this.after("CREATE", "HandlingUnitsRawMovements", (data, req) => {
-			const record = {
-				movementId: data.ID,
-				user: req.user.id,
-				tenant: req.user.tenant,
-			};
+    this.after("CREATE", "HandlingUnitsRawMovements", (data, req) => {
+      const record = {
+        CP_ID: data.CP_ID,
+        TE: data.TE,
+        TS: data.TS,
+        SSCC_ID: data.SSCC_ID,
+        DIR: data.DIR,
+        user: req.user.id,
+        tenant: req.user.tenant,
+      };
 
-			redisClient.rpush("HandlingUnitsRawMovements", JSON.stringify(record));
+      redisClient.rpush("HandlingUnitsRawMovements", JSON.stringify(record));
 
-			console.log("Inserito record:", record);
+      console.log("Inserito record:", record);
 
-			// Emettendo un throw viene eseguito un rollback sul db
-			// throw "Errore inserimento record nella lista Redis, rollback";
-		});
+      // Emettendo un throw viene eseguito un rollback sul db
+      // throw "Errore inserimento record nella lista Redis, rollback";
+    });
 
-		this.before("CREATE", "Books", (req) => {
-			this.onCommitFailed(req, this.onCommitError);
-		});
-	}
+    this.before("CREATE", "Books", (req) => {
+      this.onCommitFailed(req, this.onCommitError);
+    });
+  }
 
-	onValidationError(e, req) {
-		console.log("🤢 Validation error\n", e);
-		// console.log("🤢 Validation error - ", req.errors ? req.errors : e);
-	}
+  onValidationError(e, req) {
+    console.log("🤢 Validation error\n", e);
+    // console.log("🤢 Validation error - ", req.errors ? req.errors : e);
+  }
 
-	onCommitError(e, req) {
-		console.log("🤢 Database COMMIT error\n", e);
-	}
+  onCommitError(e, req) {
+    console.log("🤢 Database COMMIT error\n", e);
+  }
 }
 
 module.exports = HandlingUnitMoved;
