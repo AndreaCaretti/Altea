9bc63025e5f82d3f6964aaa4b263515a43f5ed34
Samuele Barzaghi <sbarzaghi@alteanet.it>
2020-10-29 00:14:18 +0100

    Tolti hardcode

--- multitenant/scripts-utility/get-cap-services-env.js
+++ multitenant/scripts-utility/get-cap-services-env.js
@@ -1,8 +1,6 @@
 const request = require("request");
 const CloudFoundryApi = require("./cloud-foundry-api");
 
-const region = "eu10";
-
 async function callService(options) {
     return new Promise((resolve, reject) => {
         request(options, function (error, response) {
@@ -12,10 +10,10 @@ async function callService(options) {
     });
 }
 
-async function getAppEnviroment(appGuid, access_token) {
+async function getAppEnviroment(apiUrl, appGuid, access_token) {
     let options = {
         method: "GET",
-        url: `https://api.cf.${region}.hana.ondemand.com/v3/apps/${appGuid}/env/`,
+        url: `${apiUrl}/v3/apps/${appGuid}/env/`,
         headers: {
             Authorization: access_token,
         },
@@ -25,12 +23,16 @@ async function getAppEnviroment(appGuid, access_token) {
     const vcap_services = JSON.parse(response.body).system_env_json.VCAP_SERVICES;
     return { VCAP_SERVICES: vcap_services };
 }
-async function main(appGuid) {
-    const access_token = await new CloudFoundryApi().getAccessToken();
-    const enviroment = await getAppEnviroment(appGuid, access_token);
+async function main(appName) {
+    const cloudFoundryApi = new CloudFoundryApi();
+    const access_token = await cloudFoundryApi.getAccessToken();
+    const appGuid = await cloudFoundryApi.getAppGuid(appName);
+    const apiUrl = await cloudFoundryApi.getApiUrl();
+
+    const enviroment = await getAppEnviroment(apiUrl, appGuid, access_token);
+
     console.log(JSON.stringify(enviroment, null, 2));
 }
 
 // GUID dell'app mtt-cap-services preso dall'url del cockpit SCP esempio:
-// https://account.eu1.hana.ondemand.com/cockpit#/globalaccount/CA12869611TID000000000741429957/subaccount/8da3ba0a-5a8a-436a-bfe7-fb7eb40a3b6e/org/d82773c3-5b40-4deb-9e9f-15c8ce1b6799/space/68ec91f6-3b26-4fca-8b8d-9c03085ab458/app/902d68b5-14dd-444b-92fd-0a6c86f33233/overview
-main("902d68b5-14dd-444b-92fd-0a6c86f33233");
+main("mtt-cap-services");
