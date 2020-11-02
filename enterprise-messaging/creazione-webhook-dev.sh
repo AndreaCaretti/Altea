361f95db5004aa9c2e4dfe2556eec5fe3ee4ace5
Samuele Barzaghi <sbarzaghi@alteanet.it>
2020-11-02 20:48:43 +0100

    Parametri per creare webhook verso CAP di sviluppo

---
+++ enterprise-messaging/creazione-webhook-dev.sh
@@ -0,0 +1,10 @@
+Subscription Name:      cat-services
+Queue Name:             coldchainplatform/customera/gateway1/handlingunits
+Quality of service:     0
+Exempt handshake:       Yes
+Webhook URL:            https://ccp-provider-dev-qas-dev-mtt-cap-services.cfapps.eu10.hana.ondemand.com/handling-unit-moved/HandlingUnitsRawMovements
+Default Content-Type:   application/json
+Authentication:         OAuth2ClientCredentials
+Client ID:              sb-ccp-provider-dev-qas-dev-cloud-cold-chain!t61773
+Client Secret:          oEPmeoq3s9kvPuGZsd89POK9Eo8=
+Token URL:              https://ccp-customera.authentication.eu10.hana.ondemand.com/oauth/token
