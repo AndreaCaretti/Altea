5f3b782a6ac06fb57f049fabdacd236bc77debde
Samuele Barzaghi <sbarzaghi@alteanet.it>
2020-11-02 09:48:23 +0100

    Non viene eseguito eslint per il file server.js Spostato il file nella cartella srv

--- multitenant/srv/server.js
+++ multitenant/srv/server.js
@@ -1,13 +1,13 @@
 const cds = require("@sap/cds");
 
-const CloudColdChain = require("./srv/CloudColdChain");
+const CloudColdChain = require("./CloudColdChain");
 const cloudColdChain = new CloudColdChain();
 
 cds.on("bootstrap", async (app) => {
     cloudColdChain.bootstrap(cds, app);
 });
 
-cds.on("served", async (app) => {
+cds.on("served", async (_app) => {
     cloudColdChain.start();
 });
 
