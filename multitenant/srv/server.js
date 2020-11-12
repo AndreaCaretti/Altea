68c12c107408035068134ebd7e4d3a9fca0f5c6d
Samuele Barzaghi <sbarzaghi@alteanet.it>
2020-11-12 01:24:14 +0100

    Se c'è un messaggio in coda da processare c'è bisogno della connessione al DB

--- multitenant/srv/server.js
+++ multitenant/srv/server.js
@@ -1,14 +1,16 @@
 const cds = require("@sap/cds");
 
 const CloudColdChain = require("./CloudColdChain");
+
 const cloudColdChain = new CloudColdChain();
 
 cds.on("bootstrap", async (app) => {
     cloudColdChain.bootstrap(cds, app);
-    cloudColdChain.start();
 });
 
-cds.on("served", async (_app) => {});
+cds.on("served", async (_app) => {
+    cloudColdChain.start();
+});
 
 // Delegate bootstrapping to built-in server.js
 module.exports = cds.server;
