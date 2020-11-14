e29118da1b20f98be99c0a394e6c023fec13d299
Samuele Barzaghi <sbarzaghi@alteanet.it>
2020-11-14 22:52:38 +0100

    Inserimento record in resident time

--- multitenant/srv/processors/processor-insert-residence-time.js
+++ multitenant/srv/processors/processor-insert-residence-time.js
@@ -1,4 +1,7 @@
 // const inputValidation = require("@sap/cds-runtime/lib/common/generic/input");
+const { selectOneField } = require("../db_utilities");
+const { selectAllWithParent } = require("../db_utilities");
+const { updateSingleField } = require("../db_utilities");
 
 const QueueResidenceTime = require("../queues/queue-residence-time");
 
@@ -16,7 +19,7 @@ class ProcessorInsertResidenceTime {
         try {
             movement = await this.queueResidenceTime.getAndSetToProcessing();
         } catch (error) {
-            console.log("Connessione redis caduta, mi rimetto in attesa");
+            this.logger.warning("Connessione redis caduta, mi rimetto in attesa");
             setImmediate(this.tick);
             return;
         }
@@ -33,20 +36,36 @@ class ProcessorInsertResidenceTime {
         const tx = cds.transaction(request);
 
         try {
-            await ProcessorInsertResidenceTime.processMovement(movement, tx);
+            const info = await this.getNecessaryInfo(movement, tx);
 
-            console.log("prima di commit");
-            const sCommit = await tx.commit();
-            console.log("dopo commit", sCommit);
+            await tx.create("ResidenceTime").entries({
+                handlingUnit_ID: movement.SSCC_ID,
+                stepNr: info.routeStep.stepNr,
+                inBusinessTime: movement.TE,
+            });
+
+            await updateSingleField(
+                "HandlingUnitsMovements",
+                movement.ID,
+                "STATUS",
+                true,
+                tx,
+                this.logger
+            );
+
+            await tx.commit();
 
             await this.queueResidenceTime.moveToComplete(movement);
         } catch (error) {
-            console.log("Error console: ", error);
-            this.logger.error("Errore inserimento record", error.toString());
-            tx.run(
-                UPDATE(cds.entities.HandlingUnitMovements)
-                    .set({ STATUS: true })
-                    .where({ ID: movement.ID })
+            this.logger.error(error.toString(), error.stack);
+
+            updateSingleField(
+                "HandlingUnitsMovements",
+                movement.ID,
+                "STATUS",
+                true,
+                tx,
+                this.logger
             );
             await tx.commit();
             await this.queueResidenceTime.moveToError(movement);
@@ -55,42 +74,42 @@ class ProcessorInsertResidenceTime {
         setImmediate(this.tick);
     }
 
-    static async processMovement(movement, tx) {
-        const lot = await ProcessorInsertResidenceTime.getLot(movement.SSCC_ID, tx);
-        const product = await ProcessorInsertResidenceTime.getProduct(lot, tx);
-        const route = await ProcessorInsertResidenceTime.getProductRoute(product, tx);
-        // eslint-disable-next-line no-unused-vars
-        const routeSteps = await ProcessorInsertResidenceTime.getRouteSteps(route, tx);
+    async getNecessaryInfo(movement, tx) {
+        const lot = await this.getLotfromSSCC(movement.SSCC_ID, tx);
+        const product = await this.getProductFromLot(lot, tx);
+        const route = await this.getRouteFromProduct(product, tx);
+        const routeSteps = await this.getRouteStepsFromRoute(route, tx);
         // const routeStep = routeSteps.find(
         //     (step) => step.controlPoint === movement.CP_ID && step.direction === movement.DIR
         // );
 
-        console.log(lot);
+        return {
+            routeStep: routeSteps[0],
+        };
     }
 
-    static async getLot(SSCC_ID, tx) {
-        const handlingUnit = await tx.run(
-            SELECT.one("HandlingUnits", ["lot_ID"]).where({ ID: SSCC_ID })
-        );
-
-        console.log("RECORD: ", handlingUnit);
-        if (!handlingUnit) {
-            throw Error("Handling unit not found");
-        }
-        return handlingUnit.lot;
+    async getLotfromSSCC(SSCC_ID, tx) {
+        this.logger.debug("getLotfromSSCC: ", SSCC_ID);
+        return selectOneField("HandlingUnits", "lot_ID", SSCC_ID, tx, this.logger);
     }
 
-    // eslint-disable-next-line no-empty-function
-    static async getProduct(_lot, _tx) {}
+    async getProductFromLot(lot, tx) {
+        this.logger.debug("getProductFromLot: ", lot);
+        return selectOneField("Lots", "product_ID", lot, tx, this.logger);
+    }
 
-    // eslint-disable-next-line no-empty-function
-    static async getProductRoute(_product, _tx) {}
+    async getRouteFromProduct(product, tx) {
+        this.logger.debug("getRouteFromProduct: ", product);
+        return selectOneField("Products", "route_ID", product, tx, this.logger);
+    }
 
-    // eslint-disable-next-line no-empty-function
-    static async getRouteSteps(_route, _tx) {}
+    async getRouteStepsFromRoute(route, tx) {
+        this.logger.debug("getRouteStepsFromRoute: ", route);
+        return selectAllWithParent("RouteSteps", route, tx, this.logger);
+    }
 
     async start() {
-        console.log(`Avvio InsertResidentTime Processor...`);
+        this.logger.debug(`Avvio InsertResidentTime Processor...`);
 
         this.queueResidenceTime.start();
 
