d520d6320ad8964771cb87d4c494ff7f52b42ecc
Samuele Barzaghi <sbarzaghi@alteanet.it>
2020-12-10 13:04:03 +0100

    Cancellazione file code non servono più

--- multitenant/srv/queues/queue-hu-raw-movements.js
+++
@@ -1,9 +0,0 @@
-const Queue = require("./internal/queue");
-
-class QueueHandlingUnitsRawMovements extends Queue {
-    constructor(logger) {
-        super("HANDLING_UNIT_RAW_MOVEMENTS", logger);
-    }
-}
-
-module.exports = QueueHandlingUnitsRawMovements;
--- multitenant/srv/queues/queue-residence-time.js
+++
@@ -1,9 +0,0 @@
-const Queue = require("./internal/queue");
-
-class QueueResidenceTime extends Queue {
-    constructor(logger) {
-        super("RESIDENT_TIME", logger);
-    }
-}
-
-module.exports = QueueResidenceTime;
