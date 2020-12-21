28de80749b4bcfd45aa7b8e2d460ea9796751358
Samuele Barzaghi <sbarzaghi@alteanet.it>
2020-12-21 12:07:49 +0100

    Se c'è un errore lasciava la cartella db/data Adesso se c'è un errore rinomina comunque db/data in db/local_data

--- multitenant/scripts-utility/update-local-db.sh
+++ multitenant/scripts-utility/update-local-db.sh
@@ -1,7 +1,12 @@
+# Attiva l'opzione che se c'è un errore deve uscire
 set -o errexit
-
 mv -v db/local_data db/data
 echo
+
+# Disattiva l'opzione che se c'è un errore deve uscire così rinomina sempre data in local_data
+set +o errexit
+
 cds deploy
+
 echo
 mv -v db/data db/local_data
